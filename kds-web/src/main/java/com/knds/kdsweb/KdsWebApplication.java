a0418b265417ebbd9cf2e6a17f1996254ae1a725
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2026-04-28 15:05:23 +0530

    first commit

---
+++ kds-web/src/main/java/com/knds/kdsweb/KdsWebApplication.java
@@ -0,0 +1,13 @@
+package com.knds.kdsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdsWebApplication.class, args);
+	}
+
+}
---
+++ kds-web/src/test/java/com/knds/kdsweb/KdsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knds.kdsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
